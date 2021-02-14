6d816fbc1ee09844983a32ac5e46f1cec7d794af
William Pepple <36817380+williampepple1@users.noreply.github.com>
2021-02-14 14:39:03 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,14 +11,16 @@ const [search, setSearch] = useState('');
 const [query, setQuery] = useState('rice')
 
 useEffect(() =>{
+const getRecipes = async () => {
+const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+const data = await response.json();
+setRecipes(data.hits);
+ }
+ 
 getRecipes();
 }, [query]);
 
-const getRecipes = async () => {
-   const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-   const data = await response.json();
-   setRecipes(data.hits);
-}
+
 const updateSearch = e => {
   setSearch(e.target.value);
   
